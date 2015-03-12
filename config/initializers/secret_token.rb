2762d9b0e5dedb908b515b6ff5330193f762aecc
Oana Theogarajan <oana.theogarajan@appfolio.com>
2015-03-12 15:37:11 -0700

    ot - Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LunchRoulette::Application.config.secret_key_base = 'd97d214758165f7ac94362253b86975c02c704bdbfadeb9cce571eac87d5f20be96035de4130e8d92d67e591d9e65107fe30bc2d219e0a8e17e48604e471883f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LunchRoulette::Application.config.session_store :cookie_store, key: '_LunchRoulette_session'
